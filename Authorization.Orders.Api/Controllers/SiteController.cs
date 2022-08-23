483d01a397c225b6627f8c1f063931975109e7f4
Artyom Puzyrev <fanny.koob@gmail.com>
2022-08-23 22:27:07 +0300

    ClientCredentials

---
+++ Authorization.Orders.Api/Controllers/SiteController.cs
@@ -0,0 +1,22 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization.Orders.Api.Controllers
+{
+    [Route("[controller]")]
+    public class SiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [Route("[action]")]
+        public string Secret()
+        {
+            return "Secret string from Orders API";
+        }
+    }
+}
---
+++ Authorization.Orders.Api/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, config =>
+    {
+        config.Authority = "https://localhost:10001";
+        config.Authority = "OrdereAPI";
+    });
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
